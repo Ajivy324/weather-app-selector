8954defa2525293719aeabfa5382a4948a7e9074
Andrew Ivy <ajivy324@yahoo.com>
2023-08-24 13:08:01 -0500

    made edits to server.js for database, fixed form

--- server/models/weather.model.js
+++ server/models/weather.model.js
@@ -1,15 +1,8 @@
 const mongoose = require("mongoose");
 
 const WeatherSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, "Weather title is required!!!"],
-        maxlength: [30, "The title's length can be no more than 30 characters!"]
-    },
-    Location: {
-        type: String,
-        required: [true, "Weather Location is required!!!"],
-    }
+    title: {type: String},
+    location: { type: String }
 }, { timestamps: true })
 
 module.exports = mongoose.model('Weather', WeatherSchema);
--- server/server.js
+++ server/server.js
@@ -4,21 +4,12 @@ const express = require("express");
 const cors = require("cors");
 const app = express();   
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require('./config/mongoose.config')
-require("./routes/weather.routes")(app);
-
-
-
-
-app.use(cors({
-    origin:"http://localhost:3000"
-}));
-
-
-
+require("./routes/weather.routes")(app)
 
 
 app.listen(8000, ()=>console.log("Listening on Port 8000"))   
